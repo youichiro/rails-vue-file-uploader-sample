3bc16308e7af6708e2869d339f076262b66507fc
youichiro <cinnamon416@gmail.com>
2020-11-15 00:19:14 +0900

    posts_controllerを作成

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,39 @@
 class PostsController < ApplicationController
+  def index
+    render json: Post.all
+  end
+
+  def show
+    render json: Post.find(params[:id])
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: post.errors, status: 422
+    end
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      render json: post
+    else
+      render json: post.errors, status: 422
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+    render json: post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :image)
+  end
 end
