0a0dc1f84772d87fcd30f22de6782820b94164cd
youichiro <cinnamon416@gmail.com>
2020-11-15 00:52:13 +0900

    routesを修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :post, only: [:index, :show, :create, :update, :destroy]
+  scope :api do
+    resources :posts, only: [:index, :show, :create, :update, :destroy]
+  end
 end
